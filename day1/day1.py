67b03c54b73a9819a1b7d9e66cfdcdc9bae4dad5
hanner-r <hrperry01@gmail.com>
2023-12-01 13:36:12 +0000

    completed code for day 1 challenge 1!

--- day1/day1.py
+++
@@ -1,6 +0,0 @@
-line_list = []
-with open('input.txt', 'r') as input_file:
-    lines = input_file.readlines()
-    for row in lines:
-        line_list.append(row)
-print(line_list)
---
+++ day1/day1_challenge1.py
@@ -0,0 +1,16 @@
+line_list = []
+with open('input.txt', 'r') as input_file:
+    lines = input_file.readlines()
+    for row in lines:
+        line_list.append(row)
+
+new_list = []
+for line in line_list:
+    new_line = [i for i in line if i.isnumeric()]
+    new_list.append(new_line)
+
+total = 0
+for i in new_list:
+    total += int(i[0] + i[-1])
+
+print(f"Total: {total}")
