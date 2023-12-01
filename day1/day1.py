3e6788cd4435e0c9f0371875d2bba9946b5f7eba
hanner-r <hrperry01@gmail.com>
2023-12-01 12:07:11 +0000

    started code for day 1 challenge 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,6 @@
-with open('thing.txt', 'r') as input_file:
-    lines = input_file.DictReader(input_file)
+line_list = []
+with open('input.txt', 'r') as input_file:
+    lines = input_file.readlines()
     for row in lines:
-        list.append(row)
+        line_list.append(row)
+print(line_list)
