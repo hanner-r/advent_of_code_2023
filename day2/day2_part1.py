7b248061d7fd6eb98b6bc94fbb21f8b5f25532e9
hanner-r <hrperry01@gmail.com>
2023-12-02 19:11:33 +0000

    started day 2 part 2

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -2,9 +2,22 @@ games_list = []
 with open('input.txt', 'r') as input_file:
     lines = input_file.readlines()
     for row in lines:
-        game = row[8::].strip('\n')
+        game = row[5::].strip('\n')
         games_list.append(game)
 
-print(games_list)
+too_many = ['13 r', '14 r', '14 g', '15', '16', '17', '18', '19', '20']
+valid_games = []
+
 for game in games_list:
-    turns = game.split(';')
+    cubes = game.split(':')
+    if not any(x in cubes[1] for x in too_many):
+        valid_games.append(game)
+
+total = 0
+for game in valid_games:
+    components = game.split(':')
+    total += int(components[0])
+
+print(f'Total: {total}')
+
+'''Completed!'''
