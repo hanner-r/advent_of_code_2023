34d89b34bd5295a0c94baba4d92d04e1806fd3d2
hanner-r <hrperry01@gmail.com>
2023-12-02 19:11:33 +0000

    started day 2 part 2

---
+++ day2/day2_part2.py
@@ -0,0 +1,12 @@
+games_list = []
+with open('input.txt', 'r') as input_file:
+    lines = input_file.readlines()
+    for row in lines:
+        game = row[8::].strip('\n')
+        games_list.append(game)
+
+print(games_list)
+for game in games_list:
+    turns = game.split(';')
+    for turn in turns:
+        grabs = turn.split(',')
